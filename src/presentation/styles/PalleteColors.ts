e9527010c5a20e484bd5c4604c74786260195a2d
Rubem <rfsv@ic.ufal.br>
2021-03-27 23:35:31 -0300

    feature: add pallete

---
+++ src/presentation/styles/PalleteColors.ts
@@ -0,0 +1,30 @@
+import { StaticLifecycle } from "react";
+
+export class PaletteColors {
+  /**
+   * @section PaletteColors
+   * @arg {StaticLifecycle} - Application
+   */
+
+  private constructor() { }
+
+
+  static $palettePrimaryDark = "#000000"
+  static $palettePrimaryMain = "#707070"
+  static $paletteSecondaryLight = "#FFFFFF"
+
+  static $paletteErrorRed50 = "#ffebee";
+  static $paletteErrorRed100 = "#ffcdd2";
+  static $paletteErrorRed200 = "#ef9a9a";
+  static $paletteErrorRed300 = "#e57373";
+  static $paletteErrorRed400 = "#ef5350";
+  static $paletteErrorRed500 = "#f44336";
+  static $paletteErrorRed600 = "#e53935";
+  static $paletteErrorRed700 = "#d32f2f";
+  static $paletteErrorRed800 = "#c62828";
+  static $paletteErrorRed900 = "#b71c1c";
+  static $paletteErrorRedA100 = "#ff8a80";
+  static $paletteErrorRedA200 = "#ff5252";
+  static $paletteErrorRedA400 = "#ff1744";
+  static $paletteErrorRedA700 = "#d50000";
+}

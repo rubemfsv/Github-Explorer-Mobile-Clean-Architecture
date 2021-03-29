cdaf70d14b3aa00ced85e4cd2dd5dfea2c3e22fb
Rubem <rfsv@ic.ufal.br>
2021-03-29 20:04:54 -0300

    feature: create input component

---
+++ src/presentation/components/Input/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useRef, useEffect } from "react";
+import { TextInputProps } from "react-native";
+import { View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/Feather";
+import { styles } from "./styles";
+
+interface IInputProps extends TextInputProps {
+  name: string;
+  icon: string;
+  placeholder?: string;
+  getInputValue: (text: string) => void;
+}
+
+const Input: React.FC<IInputProps> = ({
+  name,
+  getInputValue,
+  placeholder,
+  icon,
+}: IInputProps) => {
+  const inputElementRef = useRef<any>(null);
+  const [state, setState] = useState("");
+  const [blurred, setBlurred] = useState(true);
+
+  useEffect(() => {
+    try {
+      getInputValue(state);
+    } catch (error) {}
+  }, [state]);
+
+  const handleInputChange = (text: string): void => {
+    setState(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        ref={inputElementRef}
+        placeholderTextColor="#666360"
+        placeholder={placeholder}
+        keyboardAppearance="dark"
+        defaultValue={state}
+        onFocus={() => setBlurred(false)}
+        onBlur={() => setBlurred(true)}
+        onChangeText={(value) => {
+          handleInputChange(value);
+        }}
+      />
+    </View>
+  );
+};
+
+export default Input;
---
+++ src/presentation/components/Input/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet, Dimensions } from "react-native";
+import normalize from 'react-native-normalize'
+import { PaletteColors } from "../../styles/PalleteColors";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width * 0.7,
+    height: normalize(50),
+    alignSelf: 'center',
+    backgroundColor: PaletteColors.$paletteGrey300,
+    paddingLeft: normalize(16),
+    paddingRight: normalize(16),
+    borderBottomEndRadius: normalize(8),
+    borderBottomStartRadius: normalize(8),
+    borderTopEndRadius: normalize(8),
+    borderTopStartRadius: normalize(8),
+    marginBottom: normalize(8),
+    borderWidth: 1,
+    borderColor: PaletteColors.$palettePrimaryMain,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  textInput: {
+    flex: normalize(1),
+    color: PaletteColors.$paletteSecondaryLight,
+    fontSize: normalize(16)
+  }
+});

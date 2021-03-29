be93e95fbafe3789acbe556e509a661c26679702
Rubem <rfsv@ic.ufal.br>
2021-03-29 14:10:36 -0300

    chore: injects UserModel in RepositoryModel

--- src/domain/models/RepositoryModel.ts
+++ src/domain/models/RepositoryModel.ts
@@ -1,29 +1,12 @@
+import { UserModel } from "./UserModel";
+
 export type RepositoryModel = {
   id?: number,
   node_id?: string,
   name?: string,
   full_name?: string,
   private?: boolean,
-  owner?: {
-    login?: string,
-    id?: number,
-    node_id?: string,
-    avatar_url?: string,
-    gravatar_id?: string,
-    url?: string,
-    html_url?: string,
-    followers_url?: string,
-    following_url?: string,
-    gists_url?: string,
-    starred_url?: string,
-    subscriptions_url?: string,
-    organizations_url?: string,
-    repos_url?: string,
-    events_url?: string,
-    received_events_url?: string,
-    type?: string,
-    site_admin?: boolean
-  },
+  owner?: UserModel,
   html_url?: string,
   description?: string,
   fork?: boolean,

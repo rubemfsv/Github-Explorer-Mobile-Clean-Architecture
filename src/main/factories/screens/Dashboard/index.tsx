16f4445ec87ba37fbf211d75479ed79ae13ae9a3
Rubem <rfsv@ic.ufal.br>
2021-03-28 22:15:51 -0300

    feature: create dashboard screen feature

---
+++ src/main/factories/screens/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Dashboard } from "../../../../presentation/screens";
+
+const makeDashboard: React.FC = () => {
+  return <Dashboard />;
+};
+
+export default makeDashboard;
---
+++ src/main/factories/screens/index.ts
@@ -0,0 +1,1 @@
+export { default as makeDashboard } from './Dashboard';

ce856bb9673bb957a9577b0d259aa59645affd83
Rubem <rfsv@ic.ufal.br>
2021-03-29 23:24:26 -0300

    refactor: adjust gist model

--- src/domain/models/GistModel.ts
+++ src/domain/models/GistModel.ts
@@ -9,15 +9,7 @@ export type GistModel = {
   git_pull_url?: string,
   git_push_url?: string,
   html_url?: string,
-  files?: {
-    'gistfile1.txt'?: {
-      filename?: string,
-      type?: string,
-      language?: string,
-      raw_url?: string,
-      size?: number
-    }
-  },
+  files?: any,
   public?: boolean,
   created_at?: string,
   updated_at?: string,

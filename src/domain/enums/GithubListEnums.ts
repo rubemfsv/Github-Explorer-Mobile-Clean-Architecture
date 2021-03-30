e51608f91a2ef77ba387c7259de5a24ca9592dd7
Rubem <rfsv@ic.ufal.br>
2021-03-29 23:45:22 -0300

    feature: create GithubListEnum

---
+++ src/domain/enums/GithubListEnums.ts
@@ -0,0 +1,15 @@
+/**
+ * @enum {GithubListEnums} GithubListEnums - Definição de tipo de unidade de armazenamento
+ *
+ * @arg {repository} REPOSITORY - Repository
+ * @arg {gist} GIST - Gist
+ * @arg {follower} FOLLOWER - Follower
+ * @arg {following} FOLLOWING - Following
+ */
+
+export enum GithubListEnums {
+  REPOSITORY = 'repository',
+  GIST = 'gist',
+  FOLLOWER = 'follower',
+  FOLLOWING = 'following',
+}

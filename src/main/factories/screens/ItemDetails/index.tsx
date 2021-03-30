6657ccbfa0bb0a97d82cae50e09f3e6346ede6b7
Rubem <rfsv@ic.ufal.br>
2021-03-30 13:05:00 -0300

    feature: create ItemDetails factory page

---
+++ src/main/factories/screens/ItemDetails/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ItemDetails } from "../../../../presentation/screens";
+
+const makeItemDetails: React.FC = () => {
+  return <ItemDetails />;
+};
+
+export default makeItemDetails;
--- src/main/factories/screens/index.ts
+++ src/main/factories/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as makeHomeScreen } from './Home';
+export { default as makeItemDetails } from './ItemDetails';
 export { default as makeSearchResult } from './SearchResult';
 export { default as makeUserInfo } from './UserInfo';

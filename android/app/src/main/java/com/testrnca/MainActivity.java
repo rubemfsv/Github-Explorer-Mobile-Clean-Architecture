ff58b4051f3f8d2bccd1de17ad00aed0cb8a38db
Rubem <rfsv@ic.ufal.br>
2021-03-30 18:01:40 -0300

    refactor: change app name

--- android/app/src/main/java/com/testrnca/MainActivity.java
+++ android/app/src/main/java/com/testrnca/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testrnca;
+package com.githubexplorer;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "testrnca";
+    return "githubexplorer";
   }
 }
--- ios/testrncaTests/testrncaTests.m
+++ ios/testrncaTests/testrncaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testrncaTests : XCTestCase
+@interface githubexplorerTests : XCTestCase
 
 @end
 
-@implementation testrncaTests
+@implementation githubexplorerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

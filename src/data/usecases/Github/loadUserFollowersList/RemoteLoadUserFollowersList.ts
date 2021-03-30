7073e5548facfda4965ba1af3dd013de076a4af0
Rubem <rfsv@ic.ufal.br>
2021-03-29 22:48:12 -0300

    feature: create LoadUserFollowersList and LoadUserFollowingList data usecase

---
+++ src/data/usecases/Github/loadUserFollowersList/RemoteLoadUserFollowersList.ts
@@ -0,0 +1,30 @@
+import { HttpStatusCode, IHttpClient } from '../../../protocols/http';
+import { AccessDeniedError, UnexpectedError } from '../../../../domain/errors';
+import { UserModel } from '../../../../domain/models';
+import { ILoadUserFollowersList } from '../../../../domain/usecases';
+
+export class RemoteLoadUserFollowersList implements ILoadUserFollowersList {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: IHttpClient<UserModel[]>
+  ) { }
+
+  async loadAll(): Promise<UserModel[]> {
+    const httpResponse = await this.httpGetClient.request({
+      url: this.url,
+      method: 'get',
+    });
+    const remoteRepositoryToLoad = httpResponse.body;
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return remoteRepositoryToLoad;
+      case HttpStatusCode.noContent:
+        return httpResponse.body || null;
+      case HttpStatusCode.unauthorized:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}
--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,4 +1,6 @@
 // Github
+export * from './Github/loadUserFollowersList/RemoteLoadUserFollowersList';
+export * from './Github/loadUserFollowingList/RemoteLoadUserFollowingList';
 export * from './Github/loadUserInfo/RemoteLoadUserInfo';
 export * from './Github/loadUserRepositoryList/RemoteLoadUserRepositoryList';
 

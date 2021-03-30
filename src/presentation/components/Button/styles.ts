ba1c45431a103ee6bf24b2c414d63ae85b4453ec
Rubem <rfsv@ic.ufal.br>
2021-03-29 21:00:22 -0300

    feature: create Button component

---
+++ src/presentation/components/Button/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet, Dimensions } from "react-native";
+import normalize from 'react-native-normalize'
+import { PaletteColors } from "../../styles/PalleteColors";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width,
+    height: normalize(60),
+    alignSelf: 'center',
+    backgroundColor: PaletteColors.$paletteGrey300,
+    paddingLeft: normalize(16),
+    paddingRight: normalize(16),
+    borderBottomEndRadius: normalize(10),
+    borderBottomStartRadius: normalize(10),
+    borderTopEndRadius: normalize(10),
+    borderTopStartRadius: normalize(10),
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color:PaletteColors.$paletteGreen400,
+    fontSize: normalize(20)
+  }
+});
--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -1,2 +1,3 @@
+export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as RepositoryItem } from "./RepositoryItem";

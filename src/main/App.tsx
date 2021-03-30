e67a10843fde5fbb73c1cc117c6d69a29875d495
Rubem <rfsv@ic.ufal.br>
2021-03-30 13:04:08 -0300

    refactor: uses Palette instead of inline styles

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -3,14 +3,15 @@ import "react-native-gesture-handler";
 import React from "react";
 import { View, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { PaletteColors } from "../presentation/styles/PalleteColors";
 
 import Routes from "./routes/router";
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312E38" translucent />
+    <StatusBar barStyle="light-content" backgroundColor={PaletteColors.$paletteGrey300} translucent />
 
-    <View style={{ flex: 1, backgroundColor: "#312E38" }}>
+    <View style={{ flex: 1, backgroundColor: PaletteColors.$paletteGrey300 }}>
       <Routes />
     </View>
   </NavigationContainer>
--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { PaletteColors } from "../../presentation/styles/PalleteColors";
 
 import {
   makeHomeScreen,
+  makeItemDetails,
   makeSearchResult,
   makeUserInfo,
 } from "../factories/screens";
@@ -13,11 +15,12 @@ const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
       headerShown: false,
-      cardStyle: { backgroundColor: "#312E38" },
+      cardStyle: { backgroundColor: PaletteColors.$paletteGrey300 },
     }}
   >
     <App.Screen name="Home" component={makeHomeScreen} />
     <App.Screen name="UserInfo" component={makeUserInfo} />
+    <App.Screen name="ItemDetails" component={makeItemDetails} />
     <App.Screen name="SearchResult" component={makeSearchResult} />
   </App.Navigator>
 );

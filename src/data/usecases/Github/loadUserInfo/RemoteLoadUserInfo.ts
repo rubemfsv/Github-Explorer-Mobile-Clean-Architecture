6cd914240ba9571ecf91eebdfb21ddb7f29c8767
Rubem <rfsv@ic.ufal.br>
2021-03-30 22:17:03 -0300

    fix: adjust bugs in RemoteLoadUserInfo after test

--- src/data/usecases/Github/loadUserInfo/RemoteLoadUserInfo.ts
+++ src/data/usecases/Github/loadUserInfo/RemoteLoadUserInfo.ts
@@ -1,28 +1,31 @@
 import { HttpStatusCode, IHttpClient } from '../../../protocols/http';
 import { UnexpectedError } from '../../../../domain/errors';
-import { UserModel } from '../../../../domain/models';
 import { ILoadUserInfo } from '../../../../domain/usecases';
+import { UserModel } from '@/domain/models';
 
 export class RemoteLoadUserInfo implements ILoadUserInfo {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: IHttpClient<UserModel>
+    private readonly httpGetClient: IHttpClient<RemoteLoadUserInfo.Model>
   ) { }
 
-  async load(): Promise<UserModel> {
+  async load(): Promise<RemoteLoadUserInfo.Model> {
     const httpResponse = await this.httpGetClient.request({
       url: this.url,
       method: 'get',
     });
-    const remoteUserInfo = httpResponse.body;
+
+    const remoteUserInfo: any = httpResponse.body;
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         return remoteUserInfo;
-      case HttpStatusCode.noContent:
-        return httpResponse.body || null;
       default:
         throw new UnexpectedError();
     }
   }
+}
+
+export namespace RemoteLoadUserInfo {
+  export type Model = UserModel;
 }

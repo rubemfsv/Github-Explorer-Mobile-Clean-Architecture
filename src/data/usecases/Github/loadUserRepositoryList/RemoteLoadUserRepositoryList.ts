9ce305fa63f3ff79c406ae740b3ad7951ec9a0de
Rubem <rfsv@ic.ufal.br>
2021-03-30 23:18:20 -0300

    refactor: fix RemoteLoadUserRepository bugs after test

--- src/data/usecases/Github/loadUserRepositoryList/RemoteLoadUserRepositoryList.ts
+++ src/data/usecases/Github/loadUserRepositoryList/RemoteLoadUserRepositoryList.ts
@@ -6,23 +6,27 @@ import { ILoadUserRepositoryToList } from '../../../../domain/usecases/';
 export class RemoteLoadUserRepositoryList implements ILoadUserRepositoryToList {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: IHttpClient<RepositoryModel[]>
+    private readonly httpGetClient: IHttpClient<RemoteLoadUserRepositoryList.Model[]>
   ) { }
 
-  async loadAll(): Promise<RepositoryModel[]> {
+  async loadAll(): Promise<RemoteLoadUserRepositoryList.Model[]> {
     const httpResponse = await this.httpGetClient.request({
       url: this.url,
       method: 'get',
     });
-    const remoteRepositoryToLoad = httpResponse.body;
+    const remoteRepositoryToLoad = httpResponse.body || [];
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         return remoteRepositoryToLoad;
       case HttpStatusCode.noContent:
-        return httpResponse.body || null;
+        return [];
       default:
         throw new UnexpectedError();
     }
   }
+}
+
+export namespace RemoteLoadUserRepositoryList {
+  export type Model = RepositoryModel
 }

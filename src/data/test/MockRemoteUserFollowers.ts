67c833982afefe8cd1d219d3800a796ecb3b0d7f
Rubem <rfsv@ic.ufal.br>
2021-03-30 21:57:21 -0300

    feature: create MockRemoteUserFollower test

---
+++ src/data/test/MockRemoteUserFollowers.ts
@@ -0,0 +1,45 @@
+import { RemoteLoadUserFollowersList } from '../../data/usecases'
+
+import faker from 'faker'
+
+export const mockRemoteUserFollower = (): RemoteLoadUserFollowersList.Model => ({
+  id: faker.random.number(),
+  login: faker.random.word(),
+  node_id: faker.random.uuid(),
+  avatar_url: faker.internet.avatar(),
+  gravatar_id: faker.random.uuid(),
+  url: faker.internet.url(),
+  html_url: faker.internet.url(),
+  followers_url: faker.internet.url(),
+  following_url: faker.internet.url(),
+  gists_url: faker.internet.url(),
+  starred_url: faker.internet.url(),
+  subscriptions_url: faker.internet.url(),
+  organizations_url: faker.internet.url(),
+  repos_url: faker.internet.url(),
+  events_url: faker.internet.url(),
+  received_events_url: faker.internet.url(),
+  type: faker.random.word(),
+  site_admin: faker.random.boolean(),
+  name: faker.name.findName(),
+  company: faker.commerce.department(),
+  blog: faker.internet.url(),
+  location: faker.address.city(),
+  email: faker.internet.email(),
+  hireable: faker.random.boolean(),
+  bio: faker.name.jobArea(),
+  twitter_username: faker.internet.userName(),
+  public_repos: faker.random.number(),
+  public_gists: faker.random.number(),
+  followers: faker.random.number(),
+  following: faker.random.number(),
+  created_at: faker.date.recent().toISOString(),
+  updated_at: faker.date.recent().toISOString(),
+  pushed_at: faker.date.recent().toISOString(),
+})
+
+export const mockRemoteUserFollowerListModel = (): RemoteLoadUserFollowersList.Model[] => ([
+  mockRemoteUserFollower(),
+  mockRemoteUserFollower(),
+  mockRemoteUserFollower()
+])
--- src/data/test/index.ts
+++ src/data/test/index.ts
@@ -1,1 +1,2 @@
 export * from './MockHttp';
+export * from './MockRemoteUserFollowers';

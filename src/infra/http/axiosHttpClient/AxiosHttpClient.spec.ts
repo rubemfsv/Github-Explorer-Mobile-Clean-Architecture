0bcc6762c78379f2fae4b6b9f839d082e3069998
Rubem <rfsv@ic.ufal.br>
2021-03-30 23:39:50 -0300

    refactor: improve infra tests

--- src/infra/http/axiosHttpClient/AxiosHttpClient.spec.ts
+++ src/infra/http/axiosHttpClient/AxiosHttpClient.spec.ts
@@ -1,49 +1,49 @@
-import { AxiosHttpClient } from '../../http/axiosHttpClient/AxiosHttpClient';
-import { mockAxios, mockHttpResponse } from '../../test';
+import { AxiosHttpClient } from './AxiosHttpClient';
+import { mockAxios } from '../../test';
 import { mockHttpRequest } from '../../../data/test';
 
-import axios from 'axios';
+import axios from 'axios'
 
-jest.mock('axios');
+jest.mock('axios')
 
 type SutTypes = {
-  sut: AxiosHttpClient;
-  mockedAxios: jest.Mocked<typeof axios>;
-};
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
 
 const makeSut = (): SutTypes => {
-  const sut = new AxiosHttpClient();
-  const mockedAxios = mockAxios();
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
   return {
     sut,
-    mockedAxios,
-  };
-};
+    mockedAxios
+  }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
-    const request = mockHttpRequest();
-    const { sut, mockedAxios } = makeSut();
+    const request = mockHttpRequest()
+    const { sut, mockedAxios } = makeSut()
 
-    await sut.request(request);
+    await sut.request(request)
 
     expect(mockedAxios.request).toHaveBeenCalledWith({
       url: request.url,
       data: request.body,
       headers: request.headers,
-      method: request.method,
-    });
-  });
+      method: request.method
+    })
+  })
 
   test('Should return correct response', async () => {
-    const { sut, mockedAxios } = makeSut();
+    const { sut, mockedAxios } = makeSut()
 
-    const httpResponse = await sut.request(mockHttpRequest());
-    const axiosResponse = await mockedAxios.request.mock.results[0].value;
+    const httpResponse = await sut.request(mockHttpRequest())
+    const axiosResponse = await mockedAxios.request.mock.results[0].value
 
     expect(httpResponse).toEqual({
       statusCode: axiosResponse.status,
-      body: axiosResponse.data,
-    });
-  });
-});
+      body: axiosResponse.data
+    })
+  })
+})
--- src/infra/test/MockAxios.ts
+++ src/infra/test/MockAxios.ts
@@ -1,15 +1,13 @@
-import axios from 'axios';
-import faker from 'faker';
+import axios from 'axios'
+import faker from 'faker'
 
 export const mockHttpResponse = (): any => ({
   data: faker.random.objectElement(),
-  status: faker.random.number(),
-});
+  status: faker.random.number()
+})
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-  mockedAxios.request.mockClear().mockResolvedValue(mockHttpResponse);
-
-  return mockedAxios;
-};
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+  mockedAxios.request.mockClear().mockResolvedValue(mockHttpResponse())
+  return mockedAxios
+}

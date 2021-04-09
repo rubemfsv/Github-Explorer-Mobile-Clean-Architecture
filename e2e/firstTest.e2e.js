d7194d8ece6778eade28a4a38429e0a1b6a6f97c
Rubem <rfsv@ic.ufal.br>
2021-04-09 09:19:47 -0300

    feature: create first test to check if Detox is working

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -7,17 +7,7 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+  it('should have Github Explorer title', async () => {
+    await expect(element(by.text('Github Explorer'))).toBeVisible();
+  })
 });

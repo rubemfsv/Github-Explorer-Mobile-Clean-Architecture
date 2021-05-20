3a3df9ff9889a616cecbc9b67237d8a415fef898
Rubem <rfsv@ic.ufal.br>
2021-05-19 22:01:27 -0300

    feature: create end to end test to the search and app flow

---
+++ e2e/appTest.e2e.js
@@ -0,0 +1,55 @@
+describe("Search Flow", () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it("should render the app title", async () => {
+    // Checking if the app title is present
+    await expect(element(by.text("Github Explorer"))).toBeVisible();
+  });
+
+  it("should search an user successfully", async () => {
+    // Getting the reference to search_input and search_button
+    const search_input = element(by.id("search_user"));
+    const search_button = element(by.id("search_button"));
+
+    // Checking if the search input is visible
+    await expect(search_input).toBeVisible();
+
+    // Typing a valid user
+    await search_input.typeText("rubemfsv");
+
+    // Type \n to close keyboard
+    await search_input.typeText("\n");
+
+    // Executing a tap/press to search
+    await search_button.tap();
+
+    // Checking if found the user checking if the label public repos is showing
+    await expect(element(by.text("Public Repos"))).toBeVisible();
+
+    await expect(element(by.id("card-slide"))).swipe("right");
+
+    // Executing a tap/press on the Public Repos card
+    await element(by.text("Public Repos")).tap();
+    // Executing a tap/press to back to dashboard
+    await element(by.id("back_button")).tap();
+
+    // Executing a tap/press on the Public Gists card
+    await element(by.text("Public Gists")).tap();
+    // Executing a tap/press to back to dashboard
+    await element(by.id("back_button")).tap();
+
+    // Executing a tap/press on the Followers card
+    await element(by.text("Followers")).tap();
+    // Executing a tap/press to back to dashboard
+    await element(by.id("back_button")).tap();
+
+    await element(by.text("24")).tap();
+    await element(by.id("back_button")).tap();
+  });
+});
